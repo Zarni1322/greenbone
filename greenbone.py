b23116d65b73290ff3f98900b75c8376eee932ef
Zarni1322 <47309974+Zarni1322@users.noreply.github.com>
2023-05-22 11:40:51 +0630

    Update greenbone.py

--- greenbone.py
+++ greenbone.py
@@ -9,45 +9,49 @@ print("""
 Created by Z@rn!
     """)
 
-def package_update():
-    print("\nPackage Updating\n")
-    pkg_update = "apt update"
-    returned_value = subprocess.run(pkg_update, shell=True)
-    print('returned value:', returned_value.returncode)
+class GreenboneManager:
+    def __init__(self):
+        self.package_update()
+        self.greenbone()
+        self.feed_update()
+        self.user()
+        self.gvm_run()
 
-def greenbone():
-    print("\nInstall Greenbone Package\n")
-    greenbone_cmd = "apt install gvm"
-    returned_value = subprocess.run(greenbone_cmd, shell=True)
-    print('returned value:', returned_value.returncode)
+    def run_command(self, command):
+        returned_value = subprocess.run(command, shell=True)
+        print('Returned value:', returned_value.returncode)
 
-def feed_update():
-    print("\nFeed Updating\n")
-    feed_update_cmd = "gvm-feed-update"
-    returned_value = subprocess.run(feed_update_cmd, shell=True)
-    print('returned value:', returned_value.returncode)
+    def package_update(self):
+        print("\nPackage Updating\n")
+        pkg_update = "apt update"
+        self.run_command(pkg_update)
 
-def user():
-    user_pass = input("Do you want to set a password for admin (Y/N): ")
-    if user_pass.lower() == "y":
-        pass_set = input("Enter your new password: ")
-        pass_cmd = f"sudo -E -u _gvm gvmd --user=admin --new-password={pass_set}"
-        returned_value = subprocess.run(pass_cmd, shell=True)
-        print("Your Username is admin & Password is", pass_set)
-    else:
-        print("Bye Bye")
+    def greenbone(self):
+        print("\nInstall Greenbone Package\n")
+        greenbone_cmd = "apt install gvm"
+        self.run_command(greenbone_cmd)
 
-def gvm_run():
-    gvm_start = input("Do you want to start Greenbone (Y/N): ")
-    if gvm_start.lower() == "y":
-        gvm_run_cmd = "gvm-start"
-        returned_value = subprocess.run(gvm_run_cmd, shell=True)
-        print('returned value:', returned_value.returncode)
-    else:
-        print("Your GVM is not running!")
+    def feed_update(self):
+        print("\nFeed Updating\n")
+        feed_update_cmd = "gvm-feed-update"
+        self.run_command(feed_update_cmd)
 
-package_update()
-greenbone()
-feed_update()
-user()
-gvm_run()
+    def user(self):
+        user_pass = input("Do you want to set a password for admin (Y/N): ")
+        if user_pass.lower() == "y":
+            pass_set = input("Enter your new password: ")
+            pass_cmd = f"sudo -E -u _gvm gvmd --user=admin --new-password={pass_set}"
+            self.run_command(pass_cmd)
+            print("Your Username is admin & Password is", pass_set)
+        else:
+            print("Bye Bye")
+
+    def gvm_run(self):
+        gvm_start = input("Do you want to start Greenbone (Y/N): ")
+        if gvm_start.lower() == "y":
+            gvm_run_cmd = "gvm-start"
+            self.run_command(gvm_run_cmd)
+        else:
+            print("Your GVM is not running!")
+
+GreenboneManager()
