70a1261dff318ff91dd1cbddfe9a1f18b5b7eac2
Zarni1322 <47309974+Zarni1322@users.noreply.github.com>
2023-05-22 11:36:03 +0630

    Update greenbone.py

--- greenbone.py
+++ greenbone.py
@@ -11,41 +11,40 @@ Created by Z@rn!
 
 def package_update():
     print("\nPackage Updating\n")
-    pgk_update = "apt update"
-    returned_value = subprocess.call(pgk_update, shell=True)
-    print('returned value:', returned_value)
+    pkg_update = "apt update"
+    returned_value = subprocess.run(pkg_update, shell=True)
+    print('returned value:', returned_value.returncode)
 
 def greenbone():
     print("\nInstall Greenbone Package\n")
-    greenbone = "apt install gvm"
-    returned_value = subprocess.call(greenbone, shell=True)
-    print('returned value:', returned_value)
+    greenbone_cmd = "apt install gvm"
+    returned_value = subprocess.run(greenbone_cmd, shell=True)
+    print('returned value:', returned_value.returncode)
 
 def feed_update():
     print("\nFeed Updating\n")
-    feed_update = "gvm-feed-update"
-    returned_value = subprocess.call(feed_update, shell=True)
-    print('returned value:', returned_value)
+    feed_update_cmd = "gvm-feed-update"
+    returned_value = subprocess.run(feed_update_cmd, shell=True)
+    print('returned value:', returned_value.returncode)
 
 def user():
-    user_pass = input("Do you want to set password for admin (Y/N) : ")
-    if user_pass == "Y" or user_pass == "y":
+    user_pass = input("Do you want to set a password for admin (Y/N): ")
+    if user_pass.lower() == "y":
         pass_set = input("Enter your new password: ")
-        pass_cmd = "sudo -E -u _gvm gvmd --user=admin --new-password=" + pass_set
-        eturned_value = subprocess.call(pass_cmd, shell=True)      
+        pass_cmd = f"sudo -E -u _gvm gvmd --user=admin --new-password={pass_set}"
+        returned_value = subprocess.run(pass_cmd, shell=True)
         print("Your Username is admin & Password is", pass_set)
     else:
         print("Bye Bye")
 
 def gvm_run():
-    gvm_start=input("Do you want to start Greenbone(Y/N) : ")
-    if gvm_start == "Y" or gvm_start == "y":
-        gvm_run = "gvm-start"
-        returned_value = subprocess.call(gvm_run, shell=True)
-        print('returned value:', returned_value)
+    gvm_start = input("Do you want to start Greenbone (Y/N): ")
+    if gvm_start.lower() == "y":
+        gvm_run_cmd = "gvm-start"
+        returned_value = subprocess.run(gvm_run_cmd, shell=True)
+        print('returned value:', returned_value.returncode)
     else:
         print("Your GVM is not running!")
-
 
 package_update()
 greenbone()
